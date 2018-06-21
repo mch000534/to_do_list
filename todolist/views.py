37740e3286ec24412c95d90817a6e6743012afe1
mch000534 <34388399+mch000534@users.noreply.github.com>
2018-06-21 15:08:57 +0800

    初始

---
+++ todolist/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def home(request):
+	return render(request, 'todolist/home.html')
+
+def about(request):
+	return render(request, 'todolist/about.html')
+
+def edit(request):
+	return render(request, 'todolist/edit.html')
